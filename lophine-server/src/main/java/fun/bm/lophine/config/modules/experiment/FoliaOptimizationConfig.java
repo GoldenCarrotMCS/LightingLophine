f9fa881a95df2fc55c68119381b6f925ba6fca17
GoldenCarrotMCS <GoldenCarrotMCS@users.noreply.github.com>
2026-04-12 15:29:57 +0000

    feat: Add Folia Object Pooling patch for BlockPos

---
+++ lophine-server/src/main/java/fun/bm/lophine/config/modules/experiment/FoliaOptimizationConfig.java
@@ -0,0 +1,16 @@
+package fun.bm.lophine.config.modules.experiment;
+
+import me.earthme.luminol.config.IConfigModule;
+import me.earthme.luminol.config.flags.ConfigClassInfo;
+import me.earthme.luminol.config.flags.ConfigInfo;
+import me.earthme.luminol.enums.EnumConfigCategory;
+
+@ConfigClassInfo(category = EnumConfigCategory.EXPERIMENT, name = "folia_optimization")
+public class FoliaOptimizationConfig implements IConfigModule {
+    
+    @ConfigInfo(name = "memory.blockpos-pooling", comments = "Enable BlockPos pooling for Folia region threads")
+    public static boolean blockposPooling = true;
+
+    @ConfigInfo(name = "memory.max-blockpos-pool-size", comments = "Maximum number of cached MutableBlockPos per thread")
+    public static int maxBlockposPoolSize = 512;
+}
